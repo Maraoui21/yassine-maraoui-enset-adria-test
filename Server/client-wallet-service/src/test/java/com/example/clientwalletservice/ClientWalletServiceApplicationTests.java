3ccb709d648f9836cf62abe409192a3aebc67701
Maraoui21 <y.maraoui@etu.enset-media.ac.ma>
2023-10-30 09:13:53 +0100

    clean microservices

---
+++ Server/client-wallet-service/src/test/java/com/example/clientwalletservice/ClientWalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientwalletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientWalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
